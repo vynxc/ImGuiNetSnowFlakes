2cc57cb74a935e979c967790574c698667d20e5a
vynxc <svynxc@gmail.com>
2024-02-06 23:52:23 -0500

    w

---
+++ Snowflake/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Snowflake;
+
+Console.WriteLine("Hello, World!");
+
+var renderer = new Renderer();
+await renderer.Run();
+Console.WriteLine("Goodbye, World!");
